290546379fb5f9b26708ed749f44bbff4a79f21f
harshpatel5940 <harshpatel5940>
2021-12-10 21:10:05 +0530

    Updated TODO file

---
+++ Beta-Library/TODO.py
@@ -0,0 +1,15 @@
+print(
+    """
+# TODO | 1 | [x] Make Library for This Project for modules.py 
+# TODO | 2 | [x] (not imp) Make Individual Loops for This Project for Loop.py 
+# TODO | 3 | [x] Implement the Lib for the Project
+# TODO | 4 | [x] test it and fix all bugs
+# TODO | 5 | [ ] Make a Class for the Project Loop
+# TODO | 6 | [ ] Make Loops in the Class and initialize them
+# TODO | 7 | [ ] Make a main() function that calls the helper loops and tuns
+# TODO | 8 | [ ] Test it and fix all bugs
+\n\n"""
+)
+
+
+print("[x] = Completed | [] = Pending")
